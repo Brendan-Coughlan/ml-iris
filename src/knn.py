ead6ca90543b0e65661660af6f70983bfac33a3f
Brendan Coughlan <bcoughlan2404@gmail.com>
2026-06-26 19:02:31 -0400

    Refactored knn to use pipeline

--- src/knn.py
+++ src/knn.py
@@ -3,21 +3,20 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline
 
 iris = load_iris()
 
 X = iris.data
 y = iris.target
 
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X_scaled, y, test_size=0.2, stratify=y
+pipeline = Pipeline(
+    [("scaler", StandardScaler()), ("knn", KNeighborsClassifier(n_neighbors=5))]
 )
 
-knn = KNeighborsClassifier(n_neighbors=5)
-knn.fit(X_train, y_train)
+pipeline.fit(X_train, y_train)
 
-y_pred = knn.predict(X_test)
+y_pred = pipeline.predict(X_test)
 print(f"Test Accuracy (k=5): {accuracy_score(y_test, y_pred):.2f}")
