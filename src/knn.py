b8bbb272e0e57da21c265daabac92d651f20b078
Brendan Coughlan <bcoughlan2404@gmail.com>
2026-06-26 19:08:00 -0400

    Changed evaluation metric to cross validation

--- src/knn.py
+++ src/knn.py
@@ -1,22 +1,21 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold, cross_val_score
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
 from sklearn.pipeline import Pipeline
+import numpy as np
 
 iris = load_iris()
 
 X = iris.data
 y = iris.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
-
 pipeline = Pipeline(
     [("scaler", StandardScaler()), ("knn", KNeighborsClassifier(n_neighbors=5))]
 )
 
-pipeline.fit(X_train, y_train)
+kfold = KFold(n_splits=5, shuffle=True)
+scores = cross_val_score(pipeline, X, y, cv=kfold, scoring="accuracy")
 
-y_pred = pipeline.predict(X_test)
-print(f"Test Accuracy (k=5): {accuracy_score(y_test, y_pred):.2f}")
+print("Accuracy scores for each fold:", scores)
+print("Mean Accuracy:", np.mean(scores))
